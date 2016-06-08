9ce18d626ba90fc20bb5e6b09e23e40720122154
Ma Junbao <mjbwork12@gmail.com>
2016-06-08 23:48:46 +0800

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+// array
+// boolean
+// date
+// error
+// function
+// number
+// object
+// string
+// object
+// undefined
+// null
+// symbol
+function type(obj){
+  var result = Object.prototype.toString.call(obj).replace(/\[object /,'').replace(']','').toLowerCase();
+  if(result === 'object' && !obj){
+    return obj === null  ? 'null' : 'undefined';
+  }else{
+    return result;
+  }
+}
